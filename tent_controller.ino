8398f6eca250562de49ed4999d39b974c48380f9
Roman Shterenzon <roman.shterenzon@gmail.com>
2024-11-20 00:37:26 +0200

    heater control, icons for the win

--- tent_controller.ino
+++ tent_controller.ino
@@ -12,7 +12,8 @@ DHT dht(DHTPIN, DHTTYPE);
 
 // Define pins for relay modules
 #define FAN_RELAY_PIN 0
-#define HUMIDIFIER_RELAY_PIN 1
+#define HEATER_RELAY_PIN 1
+#define HUMIDIFIER_RELAY_PIN 2
 
 // Timing variables
 unsigned long previousMillis = 0;
@@ -23,11 +24,49 @@ const unsigned long fanOnDuration = 60000; // 1 minute in milliseconds
 const unsigned long updateInterval = 2000; // 2-second interval for main loop logic
 
 // Humidity and temperature thresholds
-float desiredHumidity = 90.0; // Desired humidity percentage
-float humidityThreshold = 4.0; // Tolerance range for humidity
+const float desiredHumidity = 90.f; // Desired humidity percentage
+const float humidityThreshold = 4.f; // Tolerance range for humidity
+const float desiredTemperature = 23.f; // 22-24
+const float temperatureThreshold = 2.f;
 
 bool isFanOn = false;
 bool isPumpOn = false;
+bool isHeaterOn = false;
+
+
+// Define the custom characters
+byte fanIcon[8] = {
+  B00100,
+  B10101,
+  B01110,
+  B10101,
+  B00100,
+  B00000,
+  B00000,
+  B00000
+};
+
+byte fireIcon[8] = {
+  B00100,
+  B01010,
+  B00101,
+  B01010,
+  B10100,
+  B01000,
+  B11100,
+  B00000
+};
+
+byte waterDropIcon[8] = {
+  B00100,
+  B00100,
+  B01110,
+  B01110,
+  B11111,
+  B11111,
+  B01110,
+  B00000
+};
 
 void setup() {
   Serial.begin(9600);
@@ -39,15 +78,20 @@ void setup() {
   lcd.init();
   lcd.backlight();  // Turn on the LCD backlight
 
+  // Create the custom characters
+  lcd.createChar(0, fanIcon);
+  lcd.createChar(1, fireIcon);
+  lcd.createChar(2, waterDropIcon);
+
   // Set relay pins as output
   pinMode(FAN_RELAY_PIN, OUTPUT);
+  pinMode(HEATER_RELAY_PIN, OUTPUT);
   pinMode(HUMIDIFIER_RELAY_PIN, OUTPUT);
 
   // Turn off the relays initially
   digitalWrite(FAN_RELAY_PIN, LOW);
+  digitalWrite(HEATER_RELAY_PIN, LOW);
   digitalWrite(HUMIDIFIER_RELAY_PIN, LOW);
-
-  mhz.setDebug(true);
 }
 
 void loop() {
@@ -73,10 +117,7 @@ void loop() {
     lcd.setCursor(0, 0);
     lcd.print("Temp: ");
     lcd.print(temperature);
-    lcd.print("C");
-
-    lcd.setCursor(0, 1);
-    lcd.print("Humidity: ");
+    lcd.print("C, Humidity: ");
     lcd.print(humidity);
     lcd.print("%");
 
@@ -87,6 +128,13 @@ void loop() {
     Serial.print(temperature);
     Serial.println("C");
 
+    lcd.setCursor(1, 1);
+    isFanOn ? lcd.write((byte)0) : lcd.print(' ');
+    lcd.setCursor(3, 1);
+    isHeaterOn ? lcd.write((byte)1) : lcd.print(' ');
+    lcd.setCursor(5, 1);
+    isPumpOn ? lcd.write((byte)2) : lcd.print(' ');
+
     // Humidifier control based on desired humidity level
     if (!isPumpOn && humidity <= (desiredHumidity - humidityThreshold)) {
       digitalWrite(HUMIDIFIER_RELAY_PIN, HIGH); // Turn on humidifier
@@ -96,6 +144,16 @@ void loop() {
       digitalWrite(HUMIDIFIER_RELAY_PIN, LOW); // Turn off humidifier
       isPumpOn = false;
       Serial.println("Humidifier OFF");
+    }
+    // Heater control based on desired temperature
+    if (!isHeaterOn && temperature <= (desiredTemperature - temperatureThreshold)) {
+      digitalWrite(HEATER_RELAY_PIN, HIGH); // Turn on heater
+      isHeaterOn = true;
+      Serial.println("Heater ON");
+    } else if (isHeaterOn && temperature >= (desiredTemperature + temperatureThreshold)) {
+      digitalWrite(HEATER_RELAY_PIN, LOW); // Turn off heater
+      isHeaterOn = false;
+      Serial.println("Heater OFF");
     }
   }
 
